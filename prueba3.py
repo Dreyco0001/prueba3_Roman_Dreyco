49f100b4f636f42e239e2861d5362f78d71cfdfe
DarvX01 <104405925+Dreyco0001@users.noreply.github.com>
2023-06-30 12:35:05 -0400

    1 subida

---
+++ prueba3.py
@@ -0,0 +1,19 @@
+#Prueba3 Dreyco Román Valdenegro
+import validaciones
+
+
+while True:
+    
+    if ¿?:
+    
+    elif ¿?:
+
+    elif ¿?:
+    
+    elif ¿?:
+
+    elif ¿?:
+
+    elif ¿?:
+
+    else:
---
+++ validaciones.py
@@ -0,0 +1,11 @@
+#nro de validacines ¿? indeterminado de momento
+import time
+
+import numpy
+
+modulos_mascotas=
+
+
+
+
+
